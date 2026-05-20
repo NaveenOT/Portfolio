be2adcd897317c824ec4a021e294725d2f93833b
Your Name <naveen.ottilingam@gmail.com>
2026-05-20 17:36:03 +0530

    cloud inference embedding v0.1

--- lib/rag/embeddings.ts
+++ lib/rag/embeddings.ts
@@ -1,6 +1,8 @@
 import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
-
-const embeddings = new HuggingFaceInferenceEmbeddings({
-  apiKey: process.env.HUGGINGFACEHUB_API_KEY!,
-  model: "sentence-transformers/all-MiniLM-L6-v2",
-});
+export  async function getEmbeddings(text: string) {
+    const embeddings = new HuggingFaceInferenceEmbeddings({
+        apiKey: process.env.HUGGINGFACEHUB_API_KEY,
+        model: "sentence-transformers/all-MiniLM-L6-v2",
+    });
+    return await embeddings.embedQuery(text);
+}

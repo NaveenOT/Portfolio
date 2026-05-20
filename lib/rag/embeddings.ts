9ec42f541c2e30cf073f1b3eaa054a9993a13144
Your Name <naveen.ottilingam@gmail.com>
2026-05-20 17:33:00 +0530

    cloud inference embedding

--- lib/rag/embeddings.ts
+++ lib/rag/embeddings.ts
@@ -1,10 +1,6 @@
-import {pipeline} from "@xenova/transformers";
-//understand code
-export  async function getEmbeddings(text: string) {
-    const embedder = await pipeline("feature-extraction", "Xenova/all-MiniLM-L6-v2");
-    const embeddings = await embedder(text, {
-        pooling: "mean",
-        normalize: true,
-    });
-    return Array.from(embeddings.data);
-}
+import { HuggingFaceInferenceEmbeddings } from "@langchain/community/embeddings/hf";
+
+const embeddings = new HuggingFaceInferenceEmbeddings({
+  apiKey: process.env.HUGGINGFACEHUB_API_KEY!,
+  model: "sentence-transformers/all-MiniLM-L6-v2",
+});
